cfda2d6f844c3bd3dc9f886efa87d359231cd280
Liang Dong <liangdong@cpp.edu>
2017-02-07 18:48:34 -0800

    done vector stack v1.0

--- App.java
+++ App.java
@@ -5,14 +5,17 @@ public class App {
 
 	public static void main(String[] args) {
 
-		ArrayStack<Integer> a = new ArrayStack<>();
-		for(int i = 0; i < 101; i++ ) {
+		VectorStack<Integer> a = new VectorStack<>();
+		for(int i = 0; i < 10; i++ ) {
 			a.push(i);
+			//System.out.println(a.pop());
+
 		}
 		for(int i = 0; i < 13; i++ ) {
-			System.out.println(a.peek());
+			//System.out.println(a.peek());
 		}
 		
+		//System.out.println(a.isEmpty());
 		a.clear();
 		a.peek();
 	}
---
+++ VectorStack.java
@@ -0,0 +1,63 @@
+package homework2;
+
+import java.util.EmptyStackException;
+import java.util.Vector;
+
+public class VectorStack<T> implements StackInterface<T>{
+	
+	Vector<T> vector;
+	private boolean initialized = false;
+	
+	public VectorStack() {
+		vector = new Vector<>();
+		initialized = true;
+	}
+	
+	
+	/** Adds a new entry to the top of this stack.
+    @param newEntry  An object to be added to the stack. */
+	public void push(T newEntry) {
+		if(checkInitialization())
+			vector.addElement(newEntry);
+	}
+
+	/** Removes and returns this stack's top entry.
+    @return  The object at the top of the stack. 
+    @throws  EmptyStackException if the stack is empty before the operation. */
+	public T pop() {
+		if (isEmpty())
+			throw new EmptyStackException();
+		else
+			return vector.remove(vector.size() - 1);		
+	}
+	
+
+	/** Retrieves this stack's top entry.
+    @return  The object at the top of the stack.
+    @throws  EmptyStackException if the stack is empty. */
+	public T peek() {
+		if (isEmpty())
+			throw new EmptyStackException();
+		else
+			return vector.get(vector.size() - 1);
+		
+	}
+
+	/** Detects whether this stack is empty.
+    @return  True if the stack is empty. */
+	public boolean isEmpty() {
+		return vector.isEmpty();
+	}
+	/** Removes all entries from this stack. */
+	public void clear() {
+		vector.clear();
+	} // end StackInterface
+
+	/** Detects whether this stack is well initialized.
+    * @return  True if the stack is well initialized. 
+    */
+	private boolean checkInitialization() {
+		return initialized;
+	}
+
+}
